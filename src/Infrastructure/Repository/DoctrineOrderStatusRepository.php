7df604601b38a32e9fa0a7717c04850cf537d2d7
Alexey Logachev <aleksei.logachev@coolclever.ru>
2024-07-17 17:49:46 +0300

    DEV Created repository interface

--- src/Infrastructure/Repository/DoctrineOrderStatusRepository.php
+++ src/Infrastructure/Repository/DoctrineOrderStatusRepository.php
@@ -5,13 +5,23 @@ declare(strict_types=1);
 namespace App\Infrastructure\Repository;
 
 use App\Domain\Entity\OrderStatus;
+use App\Domain\Repository\OrderStatusRepositoryInterface;
+use App\Domain\Repository\Query\GetStatusesByTypeQuery;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-class DoctrineOrderStatusRepository extends ServiceEntityRepository
+class DoctrineOrderStatusRepository extends ServiceEntityRepository implements OrderStatusRepositoryInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, OrderStatus::class);
+    }
+
+    /**
+     * todo Пересоздать таблицу, т.к. statusId в составе индекса не нужен
+     */
+    public function findStatusesByType(GetStatusesByTypeQuery $query): array
+    {
+        return [];
     }
 }
