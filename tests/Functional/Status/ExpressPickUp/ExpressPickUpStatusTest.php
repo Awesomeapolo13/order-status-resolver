2edcf3bdab2d1acb640dddbd4503061000e06062
Alexey Logachev <aleksei.logachev@coolclever.ru>
2024-07-29 13:14:30 +0300

    DEV Finished ExpressPickUp statuses tests.

--- tests/Functional/Status/ExpressPickUp/ExpressPickUpStatusTest.php
+++ tests/Functional/Status/ExpressPickUp/ExpressPickUpStatusTest.php
@@ -23,22 +23,22 @@ class ExpressPickUpStatusTest extends BaseOrderStatusTest
                 ExpressPickUpStatusProvider::acceptedBuildTimeExpired(),
             'Статус 1, ТТ закрыта, переход в статус 0' =>
                 ExpressPickUpStatusProvider::acceptedAfterTtClosed(),
-//            // Статус 2 и его переходы
-//            'Статус 2 без перехода (не оплачен, не нужно торопиться за заказом)' =>
-//                ExpressPickUpStatusProvider::readyUnpaid(),
-//            'Статус 2 переход в 21 (не оплачен, нужно торопиться за заказом)' =>
-//                ExpressPickUpStatusProvider::readyNeedToHurryUnpaid(),
-//            'Статус 2 переход в 22 (оплачен, не нужно торопиться за заказом)' =>
-//                ExpressPickUpStatusProvider::readyPaid(),
-//            'Статус 2 переход в 23 (оплачен, нужно торопиться за заказом)' =>
-//                ExpressPickUpStatusProvider::readyNeedToHurryPaid(),
-//            // Статус 4, изменение текста
-//            'Статус 4, можно оценить заказ' =>
-//                ExpressPickUpStatusProvider::recentlyFinished(),
-//            'Статус 4, не поставлена оценка и нельзя уже оценить' =>
-//                ExpressPickUpStatusProvider::canNotBeRatedFinished(),
-//            'Статус 4, поставлена оценка' =>
-//                ExpressPickUpStatusProvider::isRatedFinished(),
+            // Статус 2 и его переходы
+            'Статус 2 без перехода (не оплачен, не нужно торопиться за заказом)' =>
+                ExpressPickUpStatusProvider::readyUnpaid(),
+            'Статус 2 переход в 21 (не оплачен, нужно торопиться за заказом)' =>
+                ExpressPickUpStatusProvider::readyNeedToHurryUnpaid(),
+            'Статус 2 переход в 22 (оплачен, не нужно торопиться за заказом)' =>
+                ExpressPickUpStatusProvider::readyPaid(),
+            'Статус 2 переход в 23 (оплачен, нужно торопиться за заказом)' =>
+                ExpressPickUpStatusProvider::readyNeedToHurryPaid(),
+            // Статус 4, изменение текста
+            'Статус 4, можно оценить заказ' =>
+                ExpressPickUpStatusProvider::recentlyFinished(),
+            'Статус 4, не поставлена оценка и нельзя уже оценить' =>
+                ExpressPickUpStatusProvider::canNotBeRatedFinished(),
+            'Статус 4, поставлена оценка' =>
+                ExpressPickUpStatusProvider::isRatedFinished(),
         ];
     }
 }
